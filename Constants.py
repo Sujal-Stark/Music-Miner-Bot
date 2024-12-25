65e630c3e3ae6122041ef09a26d8dfed76dfd151
Sujal-Stark <sujalstark2010@gmail.com>
2024-12-25 15:58:49 +0530

    new website url added

--- Constants.py
+++ Constants.py
@@ -4,3 +4,4 @@ PAGALWORLDURL = r"https://www.pagalworld.online/" # pagal world website link
 TIMEOUT = 60 # temporary setting for letting chrome driver open until a file is downloaded
 WAITFORLOADING = 2 # explicit waiting time for a website to load
 STATUSCODE_SUCCEED = 200 # http status code for successful completion for operation
+PAGALFREESITEURL = r"https://pagalfree.com/"
