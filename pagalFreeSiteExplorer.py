9ea8f7405c52cfb538f155d59f5d33188f71dcf3
Sujal-Stark <sujalstark2010@gmail.com>
2025-03-08 04:02:41 +0530

    data extraction from search query method is developped

--- pagalFreeSiteExplorer.py
+++ pagalFreeSiteExplorer.py
@@ -32,10 +32,30 @@ class PagalFreeSiteExplorer:
             return self.soup.find_all(Constants.DIV, id = Constants.ID_CATAGORY_CONTENT)
         else:
             return [Constants.NOT_FOUND_MESSAGE]
+    
+    # separates search query elemnts and sent them in a dictionary form
+    def dataExtractFromSearchQuery(self, dataElement : list[element.Tag]) -> dict:
+        songDataContainer = {
+            Constants.LINK_TO_REDIRECT_TUNE_CONTAINER : [], # contains the link of the song
+            Constants.LINK_TO_TUNE_POSTER_CONTAINER : [], # stores the link of the poster
+            Constants.SONG_NAME : [] # stores the name of actual songs
+        }
+        for el in dataElement:
+            songDataContainer[Constants.LINK_TO_REDIRECT_TUNE_CONTAINER].append( el.find(Constants.A_TAG).get_attribute_list(Constants.HREF)[0])
+            songDataContainer[Constants.LINK_TO_TUNE_POSTER_CONTAINER].append(el.find(Constants.IMG_TAG).get_attribute_list(Constants.SRC)[0])
+            songDataContainer[Constants.SONG_NAME].append(str(el.find(Constants.DIV, class_ = Constants.MAIN_PAGE_SONG_TEXT).find(Constants.B_TAG).text).replace(" ", "").replace("\n", ""))
+        return songDataContainer
     pass
 
 # main method for testing operations
 if __name__ == '__main__':
     bot = PagalFreeSiteExplorer()
     bot.getParameterFromUser(textInput = input("Enter song name:\t"))
-    bot.searchInWebsite()
+    output = bot.dataExtractFromSearchQuery(bot.searchInWebsite())
+    for i in range(20):
+        print(
+            "iterator\t",i,"\n",
+            "Song name:\t",output[Constants.SONG_NAME][i],"\n",
+            "Song link\t", output[Constants.LINK_TO_REDIRECT_TUNE_CONTAINER][i],"\n",
+            "Image Link\t", output[Constants.LINK_TO_TUNE_POSTER_CONTAINER][i],"\n"
+        )
