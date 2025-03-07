88fdf473a68089ebdf0652114ec9408083821693
Sujal-Stark <sujalstark2010@gmail.com>
2025-03-08 03:11:40 +0530

    Values updated

--- Constants.py
+++ Constants.py
@@ -1,7 +1,17 @@
 # stores all the constants and values to work with the bot
-
-PAGALWORLDURL = r"https://www.pagalworld.online/" # pagal world website link
-TIMEOUT = 60 # temporary setting for letting chrome driver open until a file is downloaded
-WAITFORLOADING = 2 # explicit waiting time for a website to load
+# DEFAULT
 STATUSCODE_SUCCEED = 200 # http status code for successful completion for operation
+PARSER_KEY = "utf-8"
+HTML_PARSER = "html.parser"
+DIV = "div"
+
+
+# WEB SITE PARAMTERS
 PAGALFREESITEURL = r"https://pagalfree.com/"
+SEARCH_DELIMITER = "%20" # replaces the space key
+SEARCH_END_POINT = "search/" # end point for search operation in site
+ID_CATAGORY_CONTENT = "catagory_content"
+
+
+# CUSTOM
+NOT_FOUND_MESSAGE = "NOT FOUND"
