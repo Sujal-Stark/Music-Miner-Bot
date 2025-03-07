8d55bae5b75afecfa508971aee145310df7d045d
Sujal-Stark <sujalstark2010@gmail.com>
2025-03-08 04:01:33 +0530

    new constants are added

--- Constants.py
+++ Constants.py
@@ -4,14 +4,21 @@ STATUSCODE_SUCCEED = 200 # http status code for successful completion for operat
 PARSER_KEY = "utf-8"
 HTML_PARSER = "html.parser"
 DIV = "div"
-
+A_TAG = "a"
+HREF = "href"
+IMG_TAG = "img"
+SRC = "src"
+B_TAG = "b"
 
 # WEB SITE PARAMTERS
 PAGALFREESITEURL = r"https://pagalfree.com/"
 SEARCH_DELIMITER = "%20" # replaces the space key
 SEARCH_END_POINT = "search/" # end point for search operation in site
-ID_CATAGORY_CONTENT = "catagory_content"
-
+ID_CATAGORY_CONTENT = "category_content"
 
 # CUSTOM
 NOT_FOUND_MESSAGE = "NOT FOUND"
+LINK_TO_REDIRECT_TUNE_CONTAINER = "link_to_redirect_tune_container"
+LINK_TO_TUNE_POSTER_CONTAINER = "link_to_tune_poster_container"
+SONG_NAME = "song name"
+MAIN_PAGE_SONG_TEXT = "main_page_category_music_txt"
