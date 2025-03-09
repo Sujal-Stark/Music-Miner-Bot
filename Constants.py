5694af43b8357ae1fb11873797dd1a524ee01c2c
Sujal-Stark <sujalstark2010@gmail.com>
2025-03-10 03:59:25 +0530

    new Constants added

--- Constants.py
+++ Constants.py
@@ -10,6 +10,7 @@ IMG_TAG = "img"
 SRC = "src"
 B_TAG = "b"
 
+
 # WEB SITE PARAMTERS
 PAGALFREESITEURL = r"https://pagalfree.com/"
 SEARCH_DELIMITER = "%20" # replaces the space key
@@ -22,5 +23,4 @@ LINK_TO_REDIRECT_TUNE_CONTAINER = "link_to_redirect_tune_container"
 LINK_TO_TUNE_POSTER_CONTAINER = "link_to_tune_poster_container"
 SONG_NAME = "song name"
 MAIN_PAGE_SONG_TEXT = "main_page_category_music_txt"
-
-MY_CONSTANT = None
+BTN_DOWNLOAD = "btn-download"
