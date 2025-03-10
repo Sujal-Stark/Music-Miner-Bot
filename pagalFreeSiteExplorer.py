a20dd2d9295bc7dca0c6181aef5911885670a87f
Sujal-Stark <sujalstark2010@gmail.com>
2025-03-10 12:36:11 +0530

    A feature search results based upon singer name is added

--- pagalFreeSiteExplorer.py
+++ pagalFreeSiteExplorer.py
@@ -8,28 +8,45 @@ import Constants
 
 class PagalFreeSiteExplorer:
     def __init__(self):
-        self.setProperties()
+        self._setProperties()
         pass
 
     # declare properties
-    def setProperties(self) -> None:
-        self.BASE = Constants.PAGALFREESITEURL
-        self.searchQuery : str = None
-        self.soup : BeautifulSoup = None
+    def _setProperties(self) -> None:
+        '''this method is called internally while initialising the class object to instantiate necessary data structures and variable or objects'''
+        self.BASE = Constants.PAGALFREESITEURL # base url for pagalfree website
+        self.searchQuery : str = None # the name of the song shall be stored here, input given by user
+        self.soup : BeautifulSoup = None # soup content
         self.songDataContainer = {
             Constants.LINK_TO_REDIRECT_TUNE_CONTAINER : [], # contains the link of the song
             Constants.LINK_TO_TUNE_POSTER_CONTAINER : [], # stores the link of the poster
             Constants.SONG_NAME : [] # stores the name of actual songs
         }
-        self.downloadableLinks : list[str] = None
+        self.downloadableLinks : list[str] = None # actual links to download songs is stored here, none to abort
         return None
     
     def getParameterFromUser(self, textInput : str) -> None:
+        '''Take input from user as string  format and store it in self.searchQuery field'''
         self.searchQuery = textInput.replace(" ", Constants.SEARCH_DELIMITER)
         return None
     
+    def searchForSinger(self, textInput : str) -> list[str]:
+        '''takes string input from the user and returns the dictionary of list
+            data format => dict {[contains link of downlaod page], [song name], [url to the poster]}
+        '''
+        textInput = textInput.replace(" ", Constants.SEARCH_DELIMITER)
+        searchResponse = requests.get(self.BASE + Constants.SEARCH_END_POINT + textInput)
+        if(searchResponse.status_code == 200):
+            self.soup = BeautifulSoup(searchResponse.content.decode(Constants.PARSER_KEY), Constants.HTML_PARSER)
+            return self.dataExtractFromSearchQuery(self.soup.find_all(Constants.DIV, id = Constants.ID_CATAGORY_CONTENT))
+        else:
+            return [Constants.NOT_FOUND_MESSAGE]
+
     # search the query in the website if found then returns the list of the elements
     def searchInWebsite(self) -> list[element.Tag]:
+        '''searches the user query in the website and returns a web element list.
+            should be used as a argument for self.dataExtractFromSearchQuery()
+        '''
         searchRequest = requests.get(self.BASE + Constants.SEARCH_END_POINT + self.searchQuery)
         if (searchRequest.status_code == 200):
             self.soup = BeautifulSoup(
@@ -41,6 +58,9 @@ class PagalFreeSiteExplorer:
     
     # separates search query elemnts and sent them in a dictionary form
     def dataExtractFromSearchQuery(self, dataElement : list[element.Tag]) -> dict:
+        '''Takes self.searchInWebsite() as input and provides a dictionary as output
+            output format => dict {[link to download page], [song name], [url to the poster]}
+        '''
         for el in dataElement:
             self.songDataContainer[Constants.LINK_TO_REDIRECT_TUNE_CONTAINER].append( el.find(Constants.A_TAG).get_attribute_list(Constants.HREF)[0])
             self.songDataContainer[Constants.LINK_TO_TUNE_POSTER_CONTAINER].append(el.find(Constants.IMG_TAG).get_attribute_list(Constants.SRC)[0])
@@ -50,33 +70,33 @@ class PagalFreeSiteExplorer:
         return self.songDataContainer
     
     def getDownloadingUrl(self, index : int) -> None:
+        '''Takes one integer input from user to get the page link and retrieves 2 link for download'''
         try:
-            link : str = self.songDataContainer[Constants.LINK_TO_REDIRECT_TUNE_CONTAINER][index]
-            pageResponse = requests.get(link)
-            if(pageResponse.status_code == 200):
-                self.soup = BeautifulSoup(pageResponse.content.decode(Constants.PARSER_KEY), Constants.HTML_PARSER)
-                tagContainingLink : list[element.Tag] = self.soup.find_all(Constants.A_TAG, class_ = Constants.BTN_DOWNLOAD)
-                self.downloadableLinks = [a.get_attribute_list(key = Constants.HREF)[0] for a in tagContainingLink]
-            else: return
+            if self.songDataContainer:
+                pageResponse = requests.get(self.songDataContainer[Constants.LINK_TO_REDIRECT_TUNE_CONTAINER][index])
+                if(pageResponse.status_code == 200):
+                    self.soup = BeautifulSoup(pageResponse.content.decode(Constants.PARSER_KEY), Constants.HTML_PARSER)
+                    tagContainingLink : list[element.Tag] = self.soup.find_all(Constants.A_TAG, class_ = Constants.BTN_DOWNLOAD)
+                    self.downloadableLinks = [a.get_attribute_list(key = Constants.HREF)[0] for a in tagContainingLink]
         except IndexError:
             return
         return
     
     def downloadSongFromLink(self,urlIndex : int,  downloadIndex : int) -> bool:
+        '''Takes one integer index from user as 0 or 1 0 -> 180 kbps download and 1 -> 320kbps downloads'''
         try:
             self.getDownloadingUrl(urlIndex)
             if(self.downloadableLinks):
                 downloadResponse = requests.get(self.downloadableLinks[downloadIndex])
+                songName : str = self.downloadableLinks[downloadIndex].split("/")[-1]
                 if(downloadResponse.status_code == 200):
-                    with open(f"D:\Music Lib\{self.searchQuery.replace(Constants.SEARCH_DELIMITER, ' ')}.mp3", "wb") as tune:
+                    with open(f"D:\Music Lib\{songName}", "wb") as tune:
                         for chunk in downloadResponse.iter_content(chunk_size = 1024):
                             tune.write(chunk)
                     return True
-                else:
-                    return False
-            else: return False
         except IndexError:
             return False
+        return False
     pass
 
 # main method for testing operations
