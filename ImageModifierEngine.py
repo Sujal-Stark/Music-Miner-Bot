8a6b004e144cbaacd8cec80dfa74e4d36a009bfc
Sujal-Stark <sujalstark2010@gmail.com>
2025-03-18 19:43:48 +0530

    Return Type changed and errors are handled

--- ImageModifierEngine.py
+++ ImageModifierEngine.py
@@ -1,18 +1,19 @@
 # This file will be responsible for generating image objects and providing them to GUI class or other class need of using image for the design purpose or representation purpose for the application
 
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 import Constants
 
 class ImageModifier:
     @staticmethod
-    def resizeImage(imagePath : str, width : int, height = int) -> None:
-        with Image.open(imagePath) as img:
-            imgWidth, imgHeight = img.size
-            if(imgWidth != width and imgHeight != height):
-                print("worked")
-                img = img.resize((width, height))
-                img.save(imagePath)
-        return
+    def resizeImage(imagePath : str, width : int, height = int) -> bool:
+        '''Resizes the image on the given path according to the Width and Height. Weather the image is larger or smaller than the given size this method will resize it to the given size'''
+        try:
+            with Image.open(imagePath) as img:
+                if(img.size[0] != width and img.size[1] != height): # only works if the image size and given size doesn't match
+                    img = img.resize((width, height)).save(imagePath)
+        except (UnidentifiedImageError, OSError, MemoryError, TypeError, FileNotFoundError, ValueError):
+            return False
+        return True
     
 if __name__ == '__main__':
     ImageModifier.resizeImage("./static/icon.png", 128, 128)
