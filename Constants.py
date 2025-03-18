231fd119a303147c4a3d8395ad2650bd752f5f88
Sujal-Stark <sujalstark2010@gmail.com>
2025-03-18 19:31:10 +0530

    Provided contexts for constants used

--- Constants.py
+++ Constants.py
@@ -1,16 +1,19 @@
 # stores all the constants and values to work with the bot
-# DEFAULT
+
+# DEFAULT BS4 & REQUESTS RELATED CONSTANTS
 STATUSCODE_SUCCEED = 200 # http status code for successful completion for operation
-PARSER_KEY = "utf-8"
-HTML_PARSER = "html.parser"
-DIV = "div"
-A_TAG = "a"
-HREF = "href"
-IMG_TAG = "img"
-SRC = "src"
-B_TAG = "b"
-RGBA_LITERAL = "RGBA"
-RAW_LITERAL = "raw"
+PARSER_KEY = "utf-8" # convention
+HTML_PARSER = "html.parser" # convention
+RGBA_LITERAL = "RGBA" # convention
+RAW_LITERAL = "raw" # convention
+DIV = "div" # HTML tag
+A_TAG = "a" # HTML tag
+HREF = "href" # HTML tag
+IMG_TAG = "img" # HTML tag
+SRC = "src" # HTML tag
+B_TAG = "b" # HTML tag
+MAIN_PAGE_SONG_TEXT = "main_page_category_music_txt" # CSS selector
+BTN_DOWNLOAD = "btn-download" # css Selector
 
 
 # WEB SITE PARAMTERS
@@ -19,34 +22,35 @@ SEARCH_DELIMITER = "%20" # replaces the space key
 SEARCH_END_POINT = "search/" # end point for search operation in site
 ID_CATAGORY_CONTENT = "category_content"
 
-# CUSTOM
+# PROSSESSING ALGORTITHM RELATED CONSTANTS
 NOT_FOUND_MESSAGE = "NOT FOUND"
-LINK_TO_REDIRECT_TUNE_CONTAINER = "link_to_redirect_tune_container"
-LINK_TO_TUNE_POSTER_CONTAINER = "link_to_tune_poster_container"
-SONG_NAME = "song name"
-SINGER_NAME = "singer name"
-MAIN_PAGE_SONG_TEXT = "main_page_category_music_txt"
-BTN_DOWNLOAD = "btn-download"
+LINK_TO_REDIRECT_TUNE_CONTAINER = "link_to_redirect_tune_container" # dictionary key
+LINK_TO_TUNE_POSTER_CONTAINER = "link_to_tune_poster_container" # dictionary key
+SONG_NAME = "song name" # dictionary key
+SINGER_NAME = "singer name" # dictionary key
 
-# main window
-SOFTWARE_TITLE = "Music-Miner-Bot"
-SOFTWARE_WIDTH = 860
-SOFTWARE_HEIGHT = 660
+# SOFTWARE DIMENTION RELATED
+SOFTWARE_TITLE = "Music-Miner-Bot" # shows as the name
+SOFTWARE_WIDTH = 860 # main window length
+SOFTWARE_HEIGHT = 660 # main window height
+# Search Section Width and height
 SEARCH_SECTION_WIDTH = 820
 SEARCH_SECTION_HEIGHT = 80
+# control section Width and Height
 CONTROL_SECTION_WIDTH = 200
 CONTROL_SECTION_HEIGHT = 535
+# View panel Height and Width
 VIEW_PANEL_WIDTH = 610
 VIEW_PANEL_HEIGHT = CONTROL_SECTION_HEIGHT
 
-# widget titles
+# Widget Related Sconstants
 
-# search Related
+# SEARCH RELATED BUTTONS
 SEARCH_BUTTON = "Search"
 SEARCH_BY_SINGER_BUTTON =  "Search by singer's Name"
-SEARCH_HERE = "Search here 😁"
+SEARCH_HERE = "Search here....."
 
-# CONTROL RELATED
+# CONTROL RELATED BUTTON TEXTS
 CHANGE_BACKGROUND = "Change Background"
 SET_DOWNLOAD_DIRECTORY = "Set downlaoding Directory"
 SHOW_HIGH_QUALITY = "Show High Quality"
@@ -54,7 +58,7 @@ SHOW_LOW_QUALITY = "Show Low Quality"
 SHOW_DOWNLOAD_HISTORY = "Show downloading history"
 DELETE_DOWNLOAD_HISTORY = "Delete downloading history"
 
-# table Headers
+# table Headers and sizes meant to be used as constants
 SERIAL = "Serial"
 SERIAL_SIZE = 130
 THUMBNAIL = "Thumbnail"
@@ -64,13 +68,13 @@ SONG_NAME_SIZE = 150
 SINGER_NAME = "Singer name"
 SINGER_NAME_SIZE = 150
 DOWNLOAD_URL = "Download Url"
-DOWNLOAD_URL_SIZE = 115
+DOWNLOAD_URL_SIZE = 115 
 
 # Static file Paths
-ICON_PATH = R"static\icon.png"
+ICON_PATH = R"static\icon.png" # Window Icon
 TABLE_DEFAULT_LABEL = R"static\tagline.png"
-STARTING_SCREEN_PATH = R"static\startingScreen.png"
-STARTING_SCREEN_SHOW_TIME = 3
+STARTING_SCREEN_PATH = R"static\startingScreen.png" # for splash screen image
+STARTING_SCREEN_SHOW_TIME = 3 # for this much time the splash Screen will be visbile
 
 #QML path
-MAIN_QML_PATH = "masterUI.qml"
+MAIN_QML_PATH = "masterUI.qml" # Styles for Main application
