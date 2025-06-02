966b59ec3ed8f236997eeb7c159227e4f897f24a
Sujal-Stark <sujalstark2010@gmail.com>
2025-06-02 20:56:01 +0530

    Search By Singer name feature is integrated within getResults Method

--- TablePopulatorThreadClass.py
+++ TablePopulatorThreadClass.py
@@ -1,13 +1,16 @@
-# This thread class generate the QTableWidgetItems on the fly and them them as pyqt Signals
+# This thread class generate the QTableWidgetItems on the fly and them as pyqt Signals
 from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.QtGui import QPixmap
 
+# custom Imports
 from pagalFreeSiteExplorer import PagalFreeSiteExplorer
 import  Constants
 
 class TableDataStreamer(QThread):
-    dataOnFly = pyqtSignal(int, str, str, str, QPixmap) # sends signal to the main thread so that objects can be passed on
+    dataOnFly = pyqtSignal(int, str, str, str, QPixmap) # sends signal to main thread and objects are delivered
     outputSignal = pyqtSignal(str) # any operation related signal will be forwarded to the main GUI
+
+    # constructor
     def __init__(self):
         self.engine = PagalFreeSiteExplorer() # this engine scrape out the song from the website
         self._controlSignalDeclaration()
@@ -38,7 +41,7 @@ class TableDataStreamer(QThread):
 
     def _properties(self) -> None:
         """Instantiate all the necessary variables and Data structures at once"""
-        self.DOES_DATA_EXISTS: bool = False # if it's true that means no need to extract data from the first only filtering should be done
+        self.DOES_DATA_EXISTS: bool = False # if true then no need to extract data again only just filter
         self.output_object : dict = None # The output Given by Engine Will be Given here
         return
 
@@ -59,12 +62,20 @@ class TableDataStreamer(QThread):
         if not self.DOES_DATA_EXISTS:
             self.engine.searchQuery = self.searchInput
             self.output_object = None
-            if isinstance(searchOutput := self.engine.searchInWebsite(), list):
+
+            searchOutput = (
+                self.engine.searchForSinger(self.searchInput) if self.controlInputs[self.SEARCH_BY_SINGER]
+                else self.engine.searchInWebsite()
+            ) # search is done based upon singer name or song name
+
+            if isinstance(searchOutput, list) or isinstance(searchOutput, dict):
+                # search By Song returns List and Search By singer returns Dict
                 self.output_object = self.engine.dataExtractFromSearchQuery(searchOutput)
             else:
                 self.outputSignal.emit(searchOutput) # Returns message signal to the UI
                 return
             self.DOES_DATA_EXISTS = True # now data exit's no need to read again
+
         if self.output_object:
             items = len(self.output_object[Constants.SONG_NAME])
             index = 0
