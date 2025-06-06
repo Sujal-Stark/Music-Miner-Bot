0a91a8cef49d5d7c6d2ecf04627ddaa54cceb178
Sujal-Stark <sujalstark2010@gmail.com>
2025-06-07 03:31:39 +0530

    new Method getCurrentlyUsedImageLocation, clearTempDirectory method is created and imageresizeMethod is optimised for specific file remove action in temp

--- ImageModifierEngine.py
+++ ImageModifierEngine.py
@@ -1,25 +1,40 @@
 # This file will be responsible for generating image objects and providing them to GUI class or other class
 # need of using image for the design purpose or representation purpose for the application
-
 from PIL import Image, UnidentifiedImageError
 import os
 
 import Constants
 
 class ImageModifier:
-    @staticmethod
+    # properties
+    currentWallpaperLocation = None
+
+    def getCurrentlyUsedImageLocation(self, location) -> None:
+        """
+            Takes user's currently using wallpaper location and set this as class property
+            :param location: String [Valid Image location]
+            :return: None
+        """
+        self.currentWallpaperLocation = location
+        return None
+
     def resizeImage(
-            imagePath: str, width: int = Constants.SOFTWARE_WIDTH, height: int = Constants.SOFTWARE_HEIGHT,
+            self, imagePath: str, width: int = Constants.SOFTWARE_WIDTH, height: int = Constants.SOFTWARE_HEIGHT,
             dPath: str = None
     ) -> bool:
-        """Resizes the image on the given path according to the Width and Height. Weather the image is larger
-         or smaller than the given size this method will resize it to the given size"""
+        """
+            Resizes the image on the given path according to the Width and Height. Weather the image is larger
+            or smaller than the given size this method will resize it to the given size
+        """
+        # only perform delete action if there is a wallpaper location in the ./temp directory
+        if self.currentWallpaperLocation: ImageModifier.clearTempDirectory(self.currentWallpaperLocation)
         try:
             with Image.open(imagePath) as img:
-                if img.size[0] != width or img.size[1] != height:# only works if the image size and given size doesn't match
-                    if dPath: imagePath = dPath
-                    img.resize((width, height)).save(imagePath)
-        except (UnidentifiedImageError, OSError, MemoryError, TypeError, FileNotFoundError, ValueError):
+                if img.size[0] != width or img.size[1] != height:# only works if image size and given size doesn't match
+                    img = img.resize((width, height))
+                if dPath: img.save(dPath)
+                else: img.save(imagePath)
+        except (UnidentifiedImageError,OSError, TypeError, FileNotFoundError, MemoryError, ValueError):
             return False
         return True
     
@@ -44,6 +59,20 @@ class ImageModifier:
                 print("Error")
         else: pass
         return output
+
+    @staticmethod
+    def clearTempDirectory(current_file_path : str) -> None:
+        """
+            After Completing wallpaper Changing operation use this method to keep Temp folder Clean.
+        """
+        try:
+            for tempFile in os.listdir("./temp"):
+                path = os.path.join(os.getcwd() + "/temp/", tempFile).replace("\\", "/")
+                if path != current_file_path: os.remove(path)
+        except (OSError, MemoryError, TypeError, FileNotFoundError, ValueError):
+            print("Error")
+        return
     pass
 if __name__ == '__main__':
-    ImageModifier.resizeImage("./static/icon.png", 128, 128)
+    imageModifier = ImageModifier()
+    pass
