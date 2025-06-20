9c0f2f3d388a08bb1595f36efd7b8330775b4bc2
Sujal-Stark <sujalstark2010@gmail.com>
2025-06-21 00:51:26 +0530

    Crud operations are added and integrated with main application

--- DataBaseManager.py
+++ DataBaseManager.py
@@ -1,6 +1,12 @@
-from PyQt5.QtWidgets import QDialog, QApplication, QVBoxLayout, QHBoxLayout, QFrame, QLabel, QScrollArea, QTableWidget
+import os
+
+from PyQt5.QtWidgets import (QDialog, QApplication, QVBoxLayout, QHBoxLayout, QFrame, QLabel, QScrollArea,
+QTableWidget, QTableWidgetItem)
 from PyQt5.QtCore import Qt, QFile, QIODevice
-from PyQt5. QtGui import QFont
+from PyQt5. QtGui import QFont, QIcon
+from datetime import  date
+import sqlite3
+from sqlite3 import DatabaseError, OperationalError, ProgrammingError, InterfaceError
 
 import Constants
 
@@ -11,13 +17,33 @@ class DataBaseManager(QDialog):
         self.setFixedSize(Constants.DB_MANAGER_WINDOW_WIDTH, Constants.DB_MANAGER_WINDOW_HEIGHT)
         self.masterLayout = QVBoxLayout()
         self.setLayout(self.masterLayout)
+        self.setWindowIcon(QIcon(Constants.ICON_PATH))
         self.setModal(True)
 
+        self._createProperties()
         self._createUIElements()
         self._constructUI()
         self._addAttributes()
+
+        self._createDataBase()
+        self._createTableInDataBase()
+
         self._loadStyleSheet()
         return
+
+    def _createProperties(self) -> None:
+        """
+            All Properties related to class which are confidential are created here
+            :return: None
+        """
+        self.DATABASE_FILE_LOCATION = os.getcwd().replace("\\", "/") + "/Databases/historyDataBases.db"
+        self.connection : sqlite3.Connection = None # connection to the database But initially referring to NULL
+        self.dbCursor : sqlite3.Connection.cursor = None # cursor to the database
+        self.TABLE_NAME = "History"
+        self.COL_SERIAL = "serial"
+        self.COL_SONG_NAME = "SongName"
+        self.COL_DATE = "Date_of_Download"
+        return None
 
     def _createUIElements(self) -> None:
         """
@@ -59,13 +85,10 @@ class DataBaseManager(QDialog):
     def _buildTableWidget(self) -> None:
         self.table = QTableWidget()
         self.table.setColumnCount(3)
-        self.table.setHorizontalHeaderLabels(
-            [Constants.DB_TABLE_SL, Constants.DB_TABLE_SONG_NAME, Constants.DB_TABLE_DATE]
-        )
-        self.table.setColumnWidth(0, 20)
-        self.table.setColumnWidth(1, 340)
-        self.table.setColumnWidth(2, 128)
-        self.table.setRowCount(25)
+        self.table.setHorizontalHeaderLabels([Constants.DB_TABLE_SONG_NAME, Constants.DB_TABLE_DATE])
+        self.table.setColumnWidth(0, 360)
+        self.table.setColumnWidth(1, 150)
+        self.table.setRowCount(0)
         return
 
     def _buildLabels(self) -> None:
@@ -100,6 +123,101 @@ class DataBaseManager(QDialog):
         except (OSError, MemoryError, PermissionError, FileNotFoundError): return # handles loading error
 
     ###################################### INTERFACING ###############################################
+    def _createDataBase(self) -> None:
+        """
+            Creates a Database File named "historyDataBases" if not exists and creates a connection to the file
+            :return: None
+        """
+        try:
+            self.connection = sqlite3.connect(self.DATABASE_FILE_LOCATION)
+            self.dbCursor = self.connection.cursor()
+        except(DatabaseError, OperationalError, PermissionError):
+            print("Unable to Create Database")
+        return
+
+    def _createTableInDataBase(self) -> None:
+        """
+            Creates a Table inside the "historyDataBases.db" file.
+            Table will have 1. serial Number, 2. SongName, 3. Date Of Download this 3 fields
+            IF the table all ready exists it do nothing
+            :return:None
+        """
+        try:
+            if self.connection:
+                self.dbCursor.execute(
+                    f"CREATE TABLE IF NOT EXISTS {self.TABLE_NAME}(" +
+                        f"{self.COL_SERIAL} INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        f"{self.COL_SONG_NAME} TEXT," +
+                        f"{self.COL_DATE} TEXT" +
+                    ")"
+                )
+            else:
+                print("Data base doesn't exist")
+        except(ProgrammingError, InterfaceError, OperationalError, DatabaseError):
+            print("Unable to create tables")
+        return None
+
+    def insertIntoTable(self, songName : str) -> None:
+        """
+            Takes the input songName, and put it into the Table with Primary Key as Serial Number
+             and Date of download
+            :param songName: String Argument
+            :return: None
+        """
+        try:
+            if self.connection:
+                self.dbCursor.execute(
+                    f"INSERT INTO {self.TABLE_NAME}({self.COL_SONG_NAME}, {self.COL_DATE})" +
+                    f"VALUES('{songName}', '{date.today()}')"
+                )
+                self.connection.commit()
+            else:
+                print("Connection is not established")
+        except(OperationalError, DatabaseError, ProgrammingError, InterfaceError):
+            print("Unable to insert Data")
+        return None
+
+    def displayData(self) -> None:
+        """
+            Populates the Table using Data from database
+            :return: None
+        """
+        try:
+            if self.connection:
+                self.dbCursor.execute(
+                    f"SELECT * FROM {self.TABLE_NAME}"
+                )
+                rows : list = self.dbCursor.fetchall()
+                self.table.setRowCount(len(rows))
+                for rowIndex in range(len(rows)):
+                    self.table.setItem(rowIndex, 0, QTableWidgetItem(rows[rowIndex][1]))
+                    self.table.setItem(rowIndex, 1, QTableWidgetItem(rows[rowIndex][2]))
+            else:
+                print("Connection is not established")
+        except(OperationalError, DatabaseError, ProgrammingError, InterfaceError):
+            print("Unable to read data")
+        return None
+
+    def truncateTableFromDataBase(self) -> None:
+        """
+            If the table exists this Method will truncate that table
+            :return: None
+        """
+        try:
+            if self.connection:
+                self.dbCursor.execute(
+                    f"DELETE FROM {self.TABLE_NAME}"
+                )
+                self.connection.commit()
+            else:
+                print("Database doesn't exists")
+        except(ProgrammingError, InterfaceError, OperationalError, DatabaseError):
+            print("Unable to Remove data")
+        return None
+
+    def close(self):
+        self.table.setRowCount(0) # Clears The Table
+        return super().close()
     pass
 
 
